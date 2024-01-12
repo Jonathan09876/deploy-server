7704fdb962b465c3d1f939680de6e90fea6abfc5
Jonathan Jong <jonathanj09876@outlook.com>
2024-01-11 23:24:59 -0800

    #3: vercel.json

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 app.listen(
   PORT,
@@ -57,3 +57,5 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
+
+module.exports = app;
