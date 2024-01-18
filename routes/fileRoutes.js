113fc29e3b9e7978ad24622753777655c46a6d9b
Jonathan Jong <155815331+Jonathan09876@users.noreply.github.com>
2024-01-17 17:31:32 -0800

    Update fileRoutes.js

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,43 +1,36 @@
 import express from 'express'
 
 const router = express.Router()
-import {
-  authUser,
-  registerUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-} from '../controllers/userController.js'
+
 import { uploadFile } from "../controllers/uploadController.js"
-import {getAllFiles,createFiles} from "../controllers/fileController.js"
+import {getAllFiles,createFiles,deleteFiles} from "../controllers/fileController.js"
 import { protect, admin, limiter } from '../middleware/authMiddleware.js'
 import multer from 'multer'
   let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-    cb(null, '/../../../../../tmp/')
+    cb(null, './uploads')
     },
     filename: function (req, file, cb) {
     let extArray = file.mimetype.split("/");
-       console.log("aaaaa")
     let extension = extArray[extArray.length - 1];
     cb(null, file.originalname)
     }
   })
 const upload = multer({ storage: storage })
-router.route('/').post(getAllFiles).get(limiter, getUsers)
+
 router
   .route('/list')
-  .post(protect, getAllFiles)
+  .post(admin, getAllFiles)
 router
   .route('/upload')
   .post(protect,upload.single('xlsx'), uploadFile)
 
  router
   .route('/create')
-  .post(protect, createFiles)
-router
-  .route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+  .post(admin, createFiles)
+  router
+  .route('/delete')
+  .post(admin, deleteFiles)
+
 
 export default router
